a94cc46557e0d0a55adf415ccdae9617a70777ab
Yann <yann.sanquer@epitech.fr>
2022-02-20 17:40:46 +0100

    Création du reducer TaskList

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import {createStore} from "redux"
+import {createStore, combineReducers} from "redux"
 
 // Selectors: selectionner une partie de notre store
 
 // Actions: fonctions qui renvoient un objet aux reducers - {type: MON_ACTION, payload:{data}}
 
 // Reducers: fonctions retournent un nouveau state
+const initialState = [{id:1, title:"init task", completed: false}]
+const tasksList = (state = initialState, action) => {
+    switch(action.type){
+        //case MON_ACTION:
+            //modifs
+            //return newState
+        default:
+            return state
+    }
+}
+
+const rootReducers = combineReducers({
+    tasksList
+})
 
 // Store: attaché à React - accessible depuis tous les composants
 
-export const store = createStore()
+export const store = createStore(rootReducers);
